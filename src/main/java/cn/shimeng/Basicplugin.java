930c88e3be1acaa584ac0346c660a6e802331838
拯世 <wsy410526@outlook.com>
2022-06-24 23:28:15 +0800

    add fastjson,add find bedwars add findQQ

--- src/main/java/cn/shimeng/Basicplugin.java
+++ src/main/java/cn/shimeng/Basicplugin.java
@@ -1,8 +1,10 @@
 package cn.shimeng;
 
 import cn.shimeng.Listens.GroupListen;
+import net.mamoe.mirai.console.extension.PluginComponentStorage;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
+import org.jetbrains.annotations.NotNull;
 
 public final class Basicplugin extends JavaPlugin {
     public static final Basicplugin INSTANCE = new Basicplugin();
@@ -14,11 +16,16 @@ public final class Basicplugin extends JavaPlugin {
                 .build());
     }
 
-    public static GroupListen groupListen;
 
     @Override
     public void onEnable() {
-        groupListen = new GroupListen();
+        getLogger().info("Loaded!");
+
+    }
+    @Override
+    public void onLoad(@NotNull PluginComponentStorage $this$onLoad) {
+        new GroupListen();
         getLogger().info("GroupListener loaded!");
+        super.onLoad($this$onLoad);
     }
 }
--- src/main/java/cn/shimeng/Listens/GroupListen.java
+++ src/main/java/cn/shimeng/Listens/GroupListen.java
@@ -1,5 +1,8 @@
 package cn.shimeng.Listens;
 
+import cn.shimeng.utils.HttpUtils;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import net.mamoe.mirai.event.GlobalEventChannel;
 import net.mamoe.mirai.event.Listener;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
@@ -7,33 +10,126 @@ import net.mamoe.mirai.event.events.GroupMessageEvent;
 public class GroupListen {
     private long MyGroup = 921808862;
     private long TestGroup = 740061022;
+    private long NoFindQQ = 2500689407L;
     Listener listener = GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class, event -> {
+        System.out.println(event.getGroup().getId());
         if (event.getGroup().getId() == MyGroup) {
-            if (event.getMessage().contentToString().equals("查Q ")) {
-
-            }
-            if (event.getMessage().contentToString().equals("查q ")) {
-
-            }
-            //8219d452-b2a8-4580-8b09-15775eb12579
-            if (event.getMessage().contentToString().equals("/bw ")) {
-                new Thread(() -> {
-                    String[] split = event.getMessage().contentToString().split(" ");
-                    if (split.length == 2) {
-                        String name = split[1];
-
-                    }
-                });
-            }
-        } else if (event.getGroup().getId() == TestGroup) {
-            if (event.getMessage().contentToString().equals("/bw ")) {
+            if (event.getMessage().contentToString().startsWith("/bw ")) {
                 new Thread(() -> {
                     String[] split = event.getMessage().contentToString().split(" ");
                     if (split.length == 2) {
                         String name = split[1];
                         //HttpUtils.sendGet("https://api.mojang.com/users/profiles/minecraft/",)
+                        HttpUtils.HttpGetResult result = HttpUtils.getHttps("https://api.mojang.com/users/profiles/minecraft/" + name);
+                        if (result.response == 200) {
+                            JSONObject json = JSON.parseObject(result.result);
+                            HttpUtils.HttpGetResult res = HttpUtils.getHttps("https://api.hypixel.net/player?key=8219d452-b2a8-4580-8b09-15775eb12579&uuid=" + json.getString("id"));
+                            if (res.response == 200) {
+                                JSONObject json1 = JSON.parseObject(res.result);
+                                JSONObject bw = json1.getJSONObject("player").getJSONObject("stats").getJSONObject("Bedwars");
+                                int coins = bw.getIntValue("coins");
+                                int kills = bw.getIntValue("kills_bedwars");
+                                int deaths = bw.getIntValue("deaths_bedwars");
+                                String kd = String.format("%.2f", (double) ((double) kills / (double) deaths));
+                                event.getSubject().sendMessage("UserName:" + name + "\n" + "coins:" + coins + "\n" + "kills:" + kills + "\n" + "deaths:" + deaths + "\n" + "kd:" + kd);
+                            }
+                        } else if (result.response != -1) {
+                            event.getSubject().sendMessage("Error(" + result.response + "): Mojang return:" + result.result);
+                        } else {
+                            event.getSubject().sendMessage("Error:The player does not exist");
+                        }
                     }
-                });
+                }).start();
+            }
+        } else if (event.getGroup().getId() == TestGroup) {
+            if (event.getMessage().contentToString().startsWith("/bw ")) {
+                new Thread(() -> {
+                    String[] split = event.getMessage().contentToString().split(" ");
+                    if (split.length == 2) {
+                        String name = split[1];
+                        //HttpUtils.sendGet("https://api.mojang.com/users/profiles/minecraft/",)
+                        HttpUtils.HttpGetResult result = HttpUtils.getHttps("https://api.mojang.com/users/profiles/minecraft/" + name);
+                        if (result.response == 200) {
+                            JSONObject json = JSON.parseObject(result.result);
+                            HttpUtils.HttpGetResult res = HttpUtils.getHttps("https://api.hypixel.net/player?key=8219d452-b2a8-4580-8b09-15775eb12579&uuid=" + json.getString("id"));
+                            if (res.response == 200) {
+                                JSONObject json1 = JSON.parseObject(res.result);
+                                JSONObject bw = json1.getJSONObject("player").getJSONObject("stats").getJSONObject("Bedwars");
+                                int coins = bw.getIntValue("coins");
+                                int kills = bw.getIntValue("kills_bedwars");
+                                int deaths = bw.getIntValue("deaths_bedwars");
+                                String kd = String.format("%.2f", (double) ((double) kills / (double) deaths));
+                                event.getSubject().sendMessage("UserName:" + name + "\n" + "coins:" + coins + "\n" + "kills:" + kills + "\n" + "deaths:" + deaths + "\n" + "kd:" + kd);
+                            }
+                        } else if (result.response != -1) {
+                            event.getSubject().sendMessage("Error(" + result.response + "): Mojang return:" + result.result);
+                        } else {
+                            event.getSubject().sendMessage("Error:The player does not exist");
+                        }
+                    }
+                }).start();
+            }
+            if (event.getMessage().contentToString().startsWith("查Q ")) {
+                String[] split = event.getMessage().contentToString().split(" ");
+                if (split.length == 2) {
+                    new Thread(() -> {
+                        String QQ = split[1];
+                        if (QQ.equals(NoFindQQ)) {
+                            event.getSubject().sendMessage("Error");
+                        } else if (QQ.equals(event.getBot().getId())) {
+                            event.getSubject().sendMessage("Error");
+                        } else {
+                            HttpUtils.HttpGetResult result = HttpUtils.getHttps("https://qb-api.ltd/allcha.php?qq=" + QQ);
+                            if (result.response == 200) {
+                                JSONObject json = JSON.parseObject(result.result);
+                                String msg = json.getString("msg");
+                                if (msg.equals("无结果")) {
+                                    event.getSubject().sendMessage("There is no data");
+                                } else {
+                                    JSONObject data = json.getJSONObject("data");
+                                    String findQQ = data.getString("qq");
+                                    String mobilePhone = data.getString("mobile");
+                                    String LOL = data.getString("LOL");
+                                    String wb = data.getString("wb");
+                                    event.getSubject().sendMessage("QQ:" + findQQ + "\n" + "Mobile phone number:" + mobilePhone + "\n" + "LOL:" + LOL + "\n" + "weibo" + wb + "\n");
+                                }
+                            } else {
+                                event.getSubject().sendMessage("Error:The API request failed");
+                            }
+                        }
+                    }).start();
+                }
+            }
+            if (event.getMessage().contentToString().startsWith("查q ")) {
+                String[] split = event.getMessage().contentToString().split(" ");
+                if (split.length == 2) {
+                    new Thread(() -> {
+                        String QQ = split[1];
+                        if (QQ.equals(NoFindQQ)) {
+                            event.getSubject().sendMessage("Error");
+                        } else if (QQ.equals(event.getBot().getId())) {
+                            event.getSubject().sendMessage("Error");
+                        } else {
+                            HttpUtils.HttpGetResult result = HttpUtils.getHttps("https://qb-api.ltd/allcha.php?qq=" + QQ);
+                            if (result.response == 200) {
+                                JSONObject json = JSON.parseObject(result.result);
+                                String msg = json.getString("msg");
+                                if (msg.equals("无结果")) {
+                                    event.getSubject().sendMessage("There is no data");
+                                } else {
+                                    JSONObject data = json.getJSONObject("data");
+                                    String findQQ = data.getString("qq");
+                                    String mobilePhone = data.getString("mobile");
+                                    String LOL = data.getString("LOL");
+                                    String wb = data.getString("wb");
+                                    event.getSubject().sendMessage("QQ:" + findQQ + "\n" + "Mobile phone number:" + mobilePhone + "\n" + "LOL:" + LOL + "\n" + "weibo" + wb + "\n");
+                                }
+                            } else {
+                                event.getSubject().sendMessage("Error:The API request failed");
+                            }
+                        }
+                    }).start();
+                }
             }
         }
     });
